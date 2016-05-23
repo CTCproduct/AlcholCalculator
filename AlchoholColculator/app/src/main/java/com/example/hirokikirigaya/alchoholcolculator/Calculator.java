f8630514e51c324747d6490d2730fafb83254578
unknown <Hiroki Kirigaya@TU1503TU-001.ctc.tcs-group.jp>
2016-05-23 17:13:19 +0900

    メインメニュー画面の追加に伴い、コードの修正を行いました。 ビルドのみテストを行っています。

---
+++ AlchoholColculator/app/src/main/java/com/example/hirokikirigaya/alchoholcolculator/Calculator.java
@@ -0,0 +1,46 @@
+package com.example.hirokikirigaya.alchoholcolculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Hiroki Kirigaya on 2016/05/23.
+ */
+public class Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //表示画面：AlcholCalculate
+        setContentView(R.layout.alcholcalculate);
+
+        //各種Viewの取得
+        Button btncalc = (Button)findViewById(R.id.BtnCalc);
+        TextView vtxtweight = (TextView)findViewById(R.id.VtxtWeight);
+        TextView vtxtaldegree = (TextView)findViewById(R.id.VtxtAldegree);
+        TextView vtxtmili = (TextView)findViewById(R.id.VtxtMili);
+        final AutoFontSizeTextView vtxtresult = (AutoFontSizeTextView) findViewById(R.id.VtxtResult);
+        final EditText etxtweight = (EditText)findViewById(R.id.EtxtWeight);
+        final EditText etxtaldegree = (EditText)findViewById(R.id.EtxtAldegree);
+        final EditText etxtmili = (EditText)findViewById(R.id.EtxtMili);
+
+        //クラスの宣言
+        final AlcholCalculator alcholCalculator = new AlcholCalculator();
+
+        //btncalcのボタンクリック時処理
+        btncalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //1:体重より、1時間に分解できるアルコール量の計算
+                //2:飲んだ量より純アルコール量を計算する
+                //1,2の結果より分解にかかる時間を表示する
+                alcholCalculator.CalcResult(etxtweight,etxtaldegree,etxtmili,vtxtresult);
+            }
+        });
+    }
+}
--- AlchoholColculator/app/src/main/java/com/example/hirokikirigaya/alchoholcolculator/MainActivity.java
+++ AlchoholColculator/app/src/main/java/com/example/hirokikirigaya/alchoholcolculator/MainActivity.java
@@ -1,41 +1,77 @@
 package com.example.hirokikirigaya.alchoholcolculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //初期表示画面：メインメニュー
+        setContentView(R.layout.mainmenu);
 
         //各種Viewの取得
-        Button btncalc = (Button)findViewById(R.id.BtnCalc);
-        TextView vtxtweight = (TextView)findViewById(R.id.VtxtWeight);
-        TextView vtxtaldegree = (TextView)findViewById(R.id.VtxtAldegree);
-        TextView vtxtmili = (TextView)findViewById(R.id.VtxtMili);
-        final AutoFontSizeTextView vtxtresult = (AutoFontSizeTextView) findViewById(R.id.VtxtResult);
-        final EditText etxtweight = (EditText)findViewById(R.id.EtxtWeight);
-        final EditText etxtaldegree = (EditText)findViewById(R.id.EtxtAldegree);
-        final EditText etxtmili = (EditText)findViewById(R.id.EtxtMili);
+        Button btnCalc = (Button)findViewById(R.id.BtnAlcholcalc);
+        Button btnKnow = (Button)findViewById(R.id.BtnKnowledge);
+        Button btnSick = (Button)findViewById(R.id.BtnSickness);
+        Button btnStock1 = (Button) findViewById(R.id.BtnStock1);
+        Button btnSet = (Button)findViewById(R.id.BtnSetting);
+        Button btnHelp = (Button)findViewById(R.id.BtnHelp);
 
-        //クラスの宣言
-        final AlcholCalculator alcholCalculator = new AlcholCalculator();
+        //計算ボタンクリックイベント
+        btnCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),Calculator.class);
+                startActivity(intent);
 
-        //btncalcのボタンクリック時処理
-        btncalc.setOnClickListener(new View.OnClickListener() {
+            }
+        });
+
+        //知識ボタンクリックイベント
+        btnKnow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //1:体重より、1時間に分解できるアルコール量の計算
-                //2:飲んだ量より純アルコール量を計算する
-                //1,2の結果より分解にかかる時間を表示する
-                alcholCalculator.CalcResult(etxtweight,etxtaldegree,etxtmili,vtxtresult);
+            }
+        });
+
+        //2日酔いクリックイベント
+        btnSick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //予備①クリックイベント
+        btnStock1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"まだ実装されておりません。",Toast.LENGTH_SHORT);
+            }
+        });
+
+        //設定ボタンクリックイベント
+        btnSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //ヘルプボタンクリックイベント
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
