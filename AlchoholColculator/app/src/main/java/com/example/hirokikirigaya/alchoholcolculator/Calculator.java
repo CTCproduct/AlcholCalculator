2bef6e223741dc33bc6e9edd0718ea1cc078123e
unknown <Hiroki Kirigaya@TU1503TU-001.ctc.tcs-group.jp>
2016-05-24 15:45:12 +0900

    画面のカスタマイズを行いました。性別とお酒の種類、グラスの種類をSpinnerによるドロップダウン形式で表示しています。 Spinnerのカスタマイズを行う段階で、どのようなデザインがいいのかがわからずいったん作業を止めています。

--- AlchoholColculator/app/src/main/java/com/example/hirokikirigaya/alchoholcolculator/Calculator.java
+++ AlchoholColculator/app/src/main/java/com/example/hirokikirigaya/alchoholcolculator/Calculator.java
@@ -3,9 +3,13 @@ package com.example.hirokikirigaya.alchoholcolculator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 /**
  * Created by Hiroki Kirigaya on 2016/05/23.
@@ -23,13 +27,32 @@ public class Calculator extends AppCompatActivity {
         TextView vtxtweight = (TextView)findViewById(R.id.VtxtWeight);
         TextView vtxtaldegree = (TextView)findViewById(R.id.VtxtAldegree);
         TextView vtxtmili = (TextView)findViewById(R.id.VtxtMili);
-        final AutoFontSizeTextView vtxtresult = (AutoFontSizeTextView) findViewById(R.id.VtxtResult);
         final EditText etxtweight = (EditText)findViewById(R.id.EtxtWeight);
-        final EditText etxtaldegree = (EditText)findViewById(R.id.EtxtAldegree);
-        final EditText etxtmili = (EditText)findViewById(R.id.EtxtMili);
-
         //クラスの宣言
         final AlcholCalculator alcholCalculator = new AlcholCalculator();
+
+        //ListViewの設定
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Calculator.this,R.layout.spinner_main,getResources().getStringArray(R.array.sex));
+        adapter.setDropDownViewResource(R.layout.spinner_item);
+        Spinner spinner = (Spinner)findViewById(R.id.spSex);
+        spinner.setAdapter(adapter);
+
+        //項目が選択された時の動作設定
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            //ドロップダウンリストから項目が選択された時
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //テスト的にToastを表示してみる
+                Spinner spinner = (Spinner) parent;
+                Toast.makeText(getApplicationContext(),Integer.toString(spinner.getSelectedItemPosition()), Toast.LENGTH_SHORT).show();
+            }
+
+            //何も選択されなかった時の動作
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
 
         //btncalcのボタンクリック時処理
         btncalc.setOnClickListener(new View.OnClickListener() {
@@ -39,7 +62,7 @@ public class Calculator extends AppCompatActivity {
                 //1:体重より、1時間に分解できるアルコール量の計算
                 //2:飲んだ量より純アルコール量を計算する
                 //1,2の結果より分解にかかる時間を表示する
-                alcholCalculator.CalcResult(etxtweight,etxtaldegree,etxtmili,vtxtresult);
+                //alcholCalculator.CalcResult(etxtweight,etxtaldegree,etxtmili,vtxtresult);
             }
         });
     }
